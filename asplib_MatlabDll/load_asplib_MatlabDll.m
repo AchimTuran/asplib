b78de699b679bbe8478eb6ea26b53647e1a52a0f
AchimTuran <Achim.Turan@o2online.de>
2015-02-19 20:26:43 +0100

    [MatlabDll] Initial commit of matlab base files

---
+++ asplib_MatlabDll/load_asplib_MatlabDll.m
@@ -0,0 +1,47 @@
+% this script loads the asplib_Matlab.dll library
+
+%/*
+% * Copyright (C) 2014 Achim Turan, Achim.Turan@o2online.de
+% * https://github.com/AchimTuran/asplib
+% *
+% * This file is part of asplib (Achim's Signal Processing LIBrary)
+% *
+% * asplib (Achim's Signal Processing LIBrary) is free software:
+% * you can redistribute it and/or modify it under the terms of the
+% * GNU General Public License as published by the Free Software Foundation,
+% * either version 3 of the License, or (at your option) any later version.
+% *
+% * asplib (Achim's Signal Processing LIBrary) is distributed
+% * in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+% * without even the implied warranty of MERCHANTABILITY or FITNESS
+% * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+% *
+% * You should have received a copy of the GNU General Public License
+% * along with asplib (Achim's Signal Processing LIBrary).
+% * If not, see <http://www.gnu.org/licenses/>.
+% *
+% */
+
+% reset workspace
+clc
+clear all;
+
+% load asplib_MatlabDll
+addpath(fullfile(pwd,'bin')) %add bin folder to path
+dll = 'asplib_Matlab';
+dllHeader = 'asplib_MatlabDll.h';
+
+if (libisloaded('asplib_MatlabDll'))
+	disp('Reloading asplib_MatlabDll');
+	calllib('asplib_MatlabDll', 'destroy')
+	unloadlibrary('asplib_MatlabDll');
+end
+
+%hfile = fullfile(matlabroot,'extern','include','matrix.h');
+%for details of the loadlibrary function see:
+%http://www.mathworks.de/de/help/matlab/ref/loadlibrary.html
+[dllnotfound, dllwarnings] = loadlibrary(dll, dllHeader, 'alias', 'asplib_MatlabDll');
+disp('Successful loaded asplib_MatlabDll');
+
+%ToDo: call init with specific parameters
+%calllib('asplib_MatlabDll', 'init', parameters)
