d23a0a6d5794c43aaab7be8d6f7e3cb751fb4fee
AchimTuran <Achim.Turan@o2online.de>
2015-02-21 12:53:08 +0100

    [BiQuads] Initial commit of BiQuad Factory

---
+++ BiQuads/apslib_BiQuadFactory.cpp
@@ -0,0 +1,402 @@
+#pragma once
+/*
+ * Copyright (C) 2014 Achim Turan, Achim.Turan@o2online.de
+ * https://github.com/AchimTuran/asplib
+ *
+ * This file is part of asplib (Achim's Signal Processing LIBrary)
+ *
+ * asplib (Achim's Signal Processing LIBrary) is free software:
+ * you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * asplib (Achim's Signal Processing LIBrary) is distributed
+ * in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with asplib (Achim's Signal Processing LIBrary).
+ * ifnot, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+
+// this module implements basic biquad filters 
+// for more details see: http://en.wikipedia.org/wiki/Digital_biquad_filter
+
+#define _USE_MATH_DEFINES
+#include <cmath>
+
+#include "../constants_typedefs/asplib_constants.h"
+#include "../constants_typedefs/asplib_typedefs.h"
+
+#include "apslib_BiQuadFactory.h"
+#include "BiQuad_Native/asplib_BiQuad_Native.h"
+
+#include <iostream>
+using namespace std;
+
+namespace asplib
+{
+// helper function prototypes
+ASPLIB_ERR helper_calcConstQPeakingParam(ASPLIB_CONST_Q_PEAKING_PARAM *ConstQPeakingParam, ASPLIB_BIQUAD_COEFFICIENTS *Coefficients);
+ASPLIB_ERR helper_checkBiQuadIdx(ASPLIB_BIQUAD_HANDLE *BiQuads, uint BiQaudIdx);
+
+
+// Handle BiQuads
+ASPLIB_ERR CBiQuadFactory::reset_BiQuads(ASPLIB_BIQUAD_HANDLE *BiQuads)
+{
+    if(!BiQuads)
+    {
+        // ToDo: throw error!
+        return ASPLIB_ERR_INVALID_INPUT;
+    }
+
+    return ASPLIB_ERR_NO_ERROR;
+}
+
+ASPLIB_ERR CBiQuadFactory::destroy_BiQuads(ASPLIB_BIQUAD_HANDLE **BiQuads)
+{
+    if(BiQuads)
+    {
+        if((*BiQuads)->BiQuads)
+        {
+            delete (*BiQuads)->BiQuads;
+            (*BiQuads)->BiQuads = NULL;
+        }
+
+        delete *BiQuads;
+        *BiQuads = NULL;
+    }
+
+    return ASPLIB_ERR_NO_ERROR;
+}
+
+ASPLIB_BIQUAD_HANDLE* CBiQuadFactory::get_BiQuads(uint Quantity, float SampleFrequency, ASPLIB_OPT_MODULE OptModule)
+{
+    ASPLIB_BIQUAD_HANDLE *p = new ASPLIB_BIQUAD_HANDLE;
+    if(!p)
+    {
+        // ToDo: throw error!
+        return NULL;
+    }
+
+    switch(OptModule)
+    {
+        case ASPLIB_OPT_NATIVE:
+            p->optModule = OptModule;
+            p->BiQuads = new CBiQuad_Native(Quantity, SampleFrequency);
+        break;
+
+        default:
+            p->BiQuads = NULL;
+            p->optModule = ASPLIB_OPT_MIN;
+        break;
+    }
+
+    if(!p->BiQuads)
+    {
+        // ToDo: throw error!
+        // ToDo: show error: couldn't create BiQuads, becuase of dynamic memory!
+        delete p;
+        p = NULL;
+        return NULL;
+    }
+    else
+    {
+        return p;
+    }
+}
+
+ASPLIB_ERR CBiQuadFactory::calc_BiQuadSample(ASPLIB_BIQUAD_HANDLE *BiQuads, float In, float *Out)
+{
+    if(!BiQuads || !Out)
+    {
+        return ASPLIB_ERR_INVALID_INPUT;
+    }
+    
+    switch(BiQuads->optModule)
+    {
+        case ASPLIB_OPT_NATIVE:
+            *Out = static_cast<CBiQuad_Native*>(BiQuads->BiQuads)->calcSample(In);
+        break;
+
+        default:
+            // ToDo: throw error!
+            return ASPLIB_ERR_INVALID_INPUT;
+        break;
+    }
+
+    return ASPLIB_ERR_NO_ERROR;
+}
+
+ASPLIB_ERR CBiQuadFactory::calc_BiQuadSamples(ASPLIB_BIQUAD_HANDLE *BiQuads, float *In, float *Out, uint FrameSize)
+{
+    if(!BiQuads)
+    {
+        return ASPLIB_ERR_INVALID_INPUT;
+    }
+
+    ASPLIB_ERR err = ASPLIB_ERR_INVALID_INPUT;
+
+    switch(BiQuads->optModule)
+    {
+        case ASPLIB_OPT_NATIVE:
+            err = static_cast<CBiQuad_Native*>(BiQuads->BiQuads)->calcSamples(In, Out, FrameSize);
+        break;
+
+        default:
+            // ToDo: throw error!
+            return ASPLIB_ERR_INVALID_INPUT;
+        break;
+    }
+
+    return err;
+}
+
+// set BiQuad Parameters
+ASPLIB_ERR CBiQuadFactory::set_BiQuadCoefficients(ASPLIB_BIQUAD_HANDLE *BiQuads, ASPLIB_BIQUAD_COEFFICIENTS *Coefficients, float C0, float D0)
+{
+    ASPLIB_ERR err = ASPLIB_ERR_NO_ERROR;
+    if(C0 != 1.0f)
+    {
+        Coefficients->a0 *= C0;
+        Coefficients->a1 *= C0;
+        Coefficients->a2 *= C0;
+        Coefficients->b1 *= C0;
+        Coefficients->b2 *= C0;
+    }
+
+    switch(BiQuads->optModule)
+    {
+        case ASPLIB_OPT_NATIVE:
+            err = static_cast<CBiQuad_Native*>(BiQuads->BiQuads)->updateCoefficients(Coefficients, D0);
+        break;
+
+        default:
+            // ToDo: throw error! Unsupported optModule!
+            return ASPLIB_ERR_INVALID_INPUT;
+        break;
+    }
+
+
+    return ASPLIB_ERR_NO_ERROR;
+}
+
+ASPLIB_ERR CBiQuadFactory::set_BiQuadCoefficients(ASPLIB_BIQUAD_HANDLE *BiQuads, ASPLIB_BIQUAD_COEFFICIENTS *Coefficients, uint BiQuadIdx, float C0, float D0)
+{
+    ASPLIB_ERR err = ASPLIB_ERR_NO_ERROR;
+
+    if(C0 != 1.0f)
+    {
+        Coefficients->a0 *= C0;
+        Coefficients->a1 *= C0;
+        Coefficients->a2 *= C0;
+        Coefficients->b1 *= C0;
+        Coefficients->b2 *= C0;
+    }
+
+    switch(BiQuads->optModule)
+    {
+        case ASPLIB_OPT_NATIVE:
+            err = static_cast<CBiQuad_Native*>(BiQuads->BiQuads)->updateCoefficients(Coefficients, D0, BiQuadIdx);
+
+        break;
+
+        default:
+            // ToDo: throw error! Unsupported optModule!
+            return ASPLIB_ERR_INVALID_INPUT;
+        break;
+    }
+
+
+    return ASPLIB_ERR_NO_ERROR;
+}
+
+ASPLIB_ERR CBiQuadFactory::set_constQPeakingParams(ASPLIB_BIQUAD_HANDLE *BiQuads, float Gain)
+{
+    if(!BiQuads)
+    {
+        // ToDo: throw error!
+        return ASPLIB_ERR_INVALID_INPUT;
+    }
+
+    uint maxBands = 0;
+    switch(BiQuads->optModule)
+    {
+        case ASPLIB_OPT_NATIVE:
+            maxBands = static_cast<CBiQuad_Native*>(BiQuads->BiQuads)->getMaxBiquads();
+        break;
+
+        default:
+            // ToDo: throw error! Unsupported optModule!
+            return ASPLIB_ERR_INVALID_INPUT;
+        break;
+    }
+
+    ASPLIB_ERR err = ASPLIB_ERR_NO_ERROR;
+    for(uint ii = 0; ii < maxBands && err != ASPLIB_ERR_NO_ERROR; ii++)
+    {
+        err = CBiQuadFactory::set_constQPeakingParams(BiQuads, Gain);
+    }
+
+    return err;
+}
+
+ASPLIB_ERR CBiQuadFactory::set_constQPeakingParams(ASPLIB_BIQUAD_HANDLE *BiQuads, float Gain, uint BiQuadIdx)
+{
+    if(!BiQuads)
+    {
+        // ToDo: throw error!
+        return ASPLIB_ERR_INVALID_INPUT;
+    }
+
+    if(helper_checkBiQuadIdx(BiQuads, BiQuadIdx) != ASPLIB_ERR_NO_ERROR)
+    {
+        // ToDo: throw error!
+        return ASPLIB_ERR_INVALID_INPUT;
+    }
+
+    // calculate quality factor
+    float maxBands = 0.0f;
+    switch (BiQuads->optModule)
+    {
+        case ASPLIB_OPT_NATIVE:
+            maxBands = (float)static_cast<CBiQuad_Native*>(BiQuads->BiQuads)->getMaxBiquads();
+        break;
+
+        default:
+            // ToDo: throw error! Unsupported optModule!
+            return ASPLIB_ERR_INVALID_INPUT;
+        break;
+    }
+    
+    ASPLIB_CONST_Q_PEAKING_PARAM ConstQPeakingParam;
+    ConstQPeakingParam.Q = sqrtf(powf(2.0f, maxBands)) / (powf(2.0f, maxBands) - 1.0f);
+
+    // calculate center frequency of BiQuadIdx
+    // ToDo: add functions for calculated centers base 2 & 10 (ISO), preferred centers base 2 & 10 (non ISO), calculated centers - contiguous (non-ISO), Preferred centers - contiguous (non-ISO)
+    // ToDo: make maxFrequency variable and dependend from sample frequency
+    // ToDo: make baseFrequency variable
+    float baseFrequency = E_p3;         // 1000Hz
+    float maxFrequency = 20.0f*E_p3;    // 20kHz
+    float bandsFactor = maxBands / 10;
+    float positiveBands = (float)((int)(bandsFactor*log2f(maxFrequency / baseFrequency))); // round down to next complete number
+    float frequencyBandIdx = -maxBands + positiveBands +1 + BiQuadIdx;
+    ConstQPeakingParam.fc = baseFrequency*powf(2.0f, frequencyBandIdx/bandsFactor);
+
+    // get current sample frequency
+    ConstQPeakingParam.fs = 0.0f;
+    switch (BiQuads->optModule)
+    {
+        case ASPLIB_OPT_NATIVE:
+            ConstQPeakingParam.fs = static_cast<CBiQuad_Native*>(BiQuads->BiQuads)->getSampleFrequency();
+        break;
+
+        default:
+            // ToDo: throw error! Unsupported optModule!
+            return ASPLIB_ERR_INVALID_INPUT;
+        break;
+    }
+    
+
+    ASPLIB_BIQUAD_COEFFICIENTS coefficients;
+    ASPLIB_ERR err = helper_calcConstQPeakingParam(&ConstQPeakingParam, &coefficients);
+    if(err != ASPLIB_ERR_NO_ERROR)
+    {
+        // ToDo: throw error!
+        return err;
+    }
+
+    err = CBiQuadFactory::set_BiQuadCoefficients(BiQuads, &coefficients, BiQuadIdx);
+    if(err != ASPLIB_ERR_NO_ERROR)
+    {
+        // ToDo: throw error!
+        return err;
+    }
+
+    return ASPLIB_ERR_NO_ERROR;
+}
+
+//-------------------------
+//--- helper functions ----
+//-------------------------
+ASPLIB_ERR helper_calcConstQPeakingParam(ASPLIB_CONST_Q_PEAKING_PARAM *ConstQPeakingParam, ASPLIB_BIQUAD_COEFFICIENTS *Coefficients)
+{
+    if(!ConstQPeakingParam || !Coefficients || ConstQPeakingParam->fs <= 0.0f ||
+        ConstQPeakingParam->Q <= 0.0f || ConstQPeakingParam->fc <= 0.0f)
+    {
+        // ToDo: throw error!
+        return ASPLIB_ERR_INVALID_INPUT;
+    }
+
+    // ToDo: Clamp values like vlc BiQuads!
+    const float Q = ConstQPeakingParam->Q;
+    const float fc = ConstQPeakingParam->fc;
+    const float fs = ConstQPeakingParam->fs;
+    const float Gain = ConstQPeakingParam->Gain;
+
+    // ToDo: check values to avoid division by zero!
+    float K = tanf((float)M_PI*fc/fs);
+    float V0 = powf(10.0f, Gain/20.0f);
+    float d0 = 1 + K/Q + K*K;
+    float e0 = 1 + K/(Q*V0) + K*K;
+
+    float alpha = 1 + K*V0/Q + K*K;
+    float beta = 2.0f*(K*K - 1);
+    float gamma = 1 - V0*K / Q + K*K;
+    float delta = 1 - K/Q + K*K;
+    float eta = 1 - K/(V0*Q) + K*K;
+
+
+    if(Gain >= 0.0f)
+    { // boost design equations
+        Coefficients->a0 = alpha/d0;
+        Coefficients->a1 = beta/d0;
+        Coefficients->a2 = gamma/d0;
+
+        Coefficients->b1 = -beta/d0;
+        Coefficients->b2 = -delta/d0;
+    }
+    else
+    { // cut design equations
+        Coefficients->a0 = d0/e0;
+        Coefficients->a1 = beta/e0;
+        Coefficients->a2 = delta/e0;
+
+        Coefficients->b1 = -beta/e0;
+        Coefficients->b2 = -eta/e0;
+    }
+
+    return ASPLIB_ERR_NO_ERROR;
+}
+
+ASPLIB_ERR helper_checkBiQuadIdx(ASPLIB_BIQUAD_HANDLE *BiQuads, uint BiQaudIdx)
+{
+    if(!BiQuads)
+    {
+    // ToDo: throw error!
+    return ASPLIB_ERR_INVALID_INPUT;
+    }
+    
+    switch(BiQuads->optModule)
+    {
+        case ASPLIB_OPT_NATIVE:
+            if(BiQaudIdx >= static_cast<CBiQuad_Native*>(BiQuads->BiQuads)->getMaxBiquads())
+            {
+                // ToDo: throw error!
+                return ASPLIB_ERR_INVALID_INPUT;
+            }
+        break;
+
+        default:
+            // ToDo: throw error! Unsupported optModule!
+            return ASPLIB_ERR_INVALID_INPUT;
+        break;
+    }
+
+    return ASPLIB_ERR_NO_ERROR;
+}
+}
---
+++ BiQuads/apslib_BiQuadFactory.h
@@ -0,0 +1,52 @@
+#pragma once
+/*
+ * Copyright (C) 2014 Achim Turan, Achim.Turan@o2online.de
+ * https://github.com/AchimTuran/asplib
+ *
+ * This file is part of asplib (Achim's Signal Processing LIBrary)
+ *
+ * asplib (Achim's Signal Processing LIBrary) is free software:
+ * you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * asplib (Achim's Signal Processing LIBrary) is distributed
+ * in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with asplib (Achim's Signal Processing LIBrary).
+ * ifnot, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+
+// this module implements basic biquad filters 
+// for more details see: http://en.wikipedia.org/wiki/Digital_biquad_filter
+
+#include "../constants_typedefs/asplib_constants.h"
+#include "../constants_typedefs/asplib_typedefs.h"
+
+namespace asplib
+{
+class CBiQuadFactory
+{
+public:
+    // Handle BiQuads
+    static ASPLIB_ERR            reset_BiQuads(ASPLIB_BIQUAD_HANDLE *BiQuads);
+    static ASPLIB_ERR            destroy_BiQuads(ASPLIB_BIQUAD_HANDLE **BiQuads);
+    static ASPLIB_BIQUAD_HANDLE* get_BiQuads(uint BiQuadQuantity, float SampleFrequency, ASPLIB_OPT_MODULE OptModule);
+    static ASPLIB_ERR            calc_BiQuadSample(ASPLIB_BIQUAD_HANDLE *BiQuads, float In, float *Out);
+    static ASPLIB_ERR            calc_BiQuadSamples(ASPLIB_BIQUAD_HANDLE *BiQuads, float *In, float *Out, uint FrameSize);
+
+    // set BiQuad Parameters
+    static ASPLIB_ERR            set_BiQuadCoefficients(ASPLIB_BIQUAD_HANDLE *BiQuads, ASPLIB_BIQUAD_COEFFICIENTS *Coefficients, float C0 = 1.0f, float D0 = 0.0f);
+    static ASPLIB_ERR            set_BiQuadCoefficients(ASPLIB_BIQUAD_HANDLE *BiQuads, ASPLIB_BIQUAD_COEFFICIENTS *Coefficients, uint BiQuadIdx, float C0 = 1.0f, float D0 = 0.0f);
+    // Octave EQs, 1 Octave (9 to 11 bands), 2/3 octave (15 - 17 bands),  1/2 octave (17 to 22 bands), and 1/3 octave (30 - 31 bands)
+    // frequency bands are defined in ISO R 266-1997 or ANSI S1.6-1984
+    static ASPLIB_ERR            set_constQPeakingParams(ASPLIB_BIQUAD_HANDLE *BiQuads, float Gain);
+    static ASPLIB_ERR            set_constQPeakingParams(ASPLIB_BIQUAD_HANDLE *BiQuads, float Gain, uint BiQuadIdx);
+};
+}
