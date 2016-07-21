97d8dde7a8d44e8e81b47b50c8216b10239d8b7c
AchimTuran <mail@achim-turan.de>
2016-07-21 23:13:54 +0200

    [CMake] New folder structure <module>/asplib/<module>

--- cmake/asplib_helpers.cmake
+++ cmake/asplib_helpers.cmake
@@ -11,6 +11,8 @@ function(asplib_source_group FILE_LIST)
       get_filename_component(FILEDIRECTORY ${_FILE} DIRECTORY)
     endif()
 
+    string(REPLACE "asplib/" "" FILEDIRECTORY "${FILEDIRECTORY}")
+
     #message(STATUS "_FILE=${_FILE}")
     if(NOT "${FILEDIRECTORY}" STREQUAL "")
       string(REPLACE "/" "\\" FILE_FILEGROUP ${FILEDIRECTORY})
@@ -37,7 +39,7 @@ endfunction()
 function(asplib_install_module MODULE_NAME FILE_LIST FOLDER)
   foreach(_FILE ${FILE_LIST})
     # remove absolute path to this module
-    string(REPLACE "${CMAKE_SOURCE_DIR}/asplib_modules/${MODULE_NAME}/" "" _FILE "${_FILE}")
+    string(REPLACE "${CMAKE_SOURCE_DIR}/asplib_modules/${MODULE_NAME}/asplib/" "" _FILE "${_FILE}")
     get_filename_component(HEADERDIR ${_FILE} DIRECTORY)
         
     install(FILES ${CMAKE_CURRENT_SOURCE_DIR}/${_FILE} DESTINATION ${FOLDER}/${HEADERDIR})
