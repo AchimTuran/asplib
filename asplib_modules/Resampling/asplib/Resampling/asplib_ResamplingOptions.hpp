011789364d02308aa5b2ee0380499c08030d72b9
AchimTuran <mail@achim-turan.de>
2016-12-17 08:29:53 +0100

    [asplib_modules][Resampling] Initial commit of resampling factory implementation

--- asplib_modules/Resampling/asplib/Resampling/asplib_ResamplingOptions.hpp
+++ asplib_modules/Resampling/asplib/Resampling/asplib_ResamplingOptions.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-/* Copyright (C) 2014-2015 Achim Turan, Achim.Turan@o2online.de
+/* Copyright (C) 2014-2016 Achim Turan, Achim.Turan@o2online.de
  * https://github.com/AchimTuran/asplib
  *
  * This file is part of asplib (Achim's Signal Processing LIBrary)
@@ -26,29 +26,4 @@
 #include "Core/os/asplib_os.h"
 #include "Core/Constants_Typedefs/asplib_Typedefs.h"
 
-
-namespace asplib
-{
-typedef enum
-{
-  ASPLIB_EXTENDED_STRUC_INVALID_ID = -1,
-
-  ASPLIB_EXTENDED_STRUCT_KissFFTRealOptions,
-  ASPLIB_EXTENDED_STRUCT_KissFFTCpxOptions,
-
-  // scaler options
-  ASPLIB_EXTENDED_STRUCT_TLog10ScalerOptions,
-  
-  // FFT smoother options
-  ASPLIB_EXTENDED_STRUCT_dlbeerSmootherOptions,
-  ASPLIB_EXTENDED_STRUCT_TAttackReleaseSmootherOptions,
-
-  // FFT remapper options
-  ASPLIB_EXTENDED_STRUCT_TGammaCorrectorOptions,
-
-  // Resampling options
-  ASPLIB_EXTENDED_STRUCT_DecimatorOptions,
-
-  ASPLIB_EXTENDED_STRUC_MAX_ID = -1
-}asplibExtendedStructIDs_t;
-}
+#include "Resampling/Decimator/asplib_DecimatorOptions.hpp"
