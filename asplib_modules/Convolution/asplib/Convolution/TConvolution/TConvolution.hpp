7825ab63bf8f05e4f90c92e5ec662edc1383b0ea
AchimTuran <mail@achim-turan.de>
2016-12-22 21:12:07 +0100

    [asplib_modules][Convolution] First working prototype implementation for fast convolution

--- asplib_modules/Convolution/asplib/Convolution/TConvolution/TConvolution.hpp
+++ asplib_modules/Convolution/asplib/Convolution/TConvolution/TConvolution.hpp
@@ -38,48 +38,89 @@ namespace asplib
 template<class T>
 class TConvolution : public IConvolution
 {
-  asplibFmt_t internalFmt = ASPLIB_FMT_NATIVE_CPX_FLOAT;
   asplibFmt_t FFTInFmt = ASPLIB_FMT_NATIVE_FLOAT;
 
 public:
-  TConvolution() {}
-  ~TConvolution() {}
-
-  virtual ASPLIB_ERR Create(uint32_t FrameSize, FFTLibID_t FFTLibID, void *Filter, uint32_t FilterLength, uint32_t MaxParts, void *Options = nullptr)
+  TConvolution()
   {
-    IFFT *fft = nullptr;
-    ASPLIB_ERR err = ASPLIB_FACTORY_FFT.Create(FFTLibID, FFTInFmt, internalFmt, fft);
+    m_G = nullptr;
+    m_X = nullptr;
+    m_Y = nullptr;
+    m_y = nullptr;
+    m_x = nullptr;
+
+    m_fft = nullptr;
+
+    m_ScaleValue          = 1.0;
+
+    m_MaxParts            = 0;
+    m_MaxFrameSize        = 0;
+    m_MaxInteralFrameSize = 0;
+    m_MaxVectorFrameSize  = 0;
+  }
+  ~TConvolution() { Destroy(); }
+
+  virtual ASPLIB_ERR Create(uint32_t FrameSize, FFTLibID_t FFTLibID, void *Filter, uint32_t FilterLength, void *Options = nullptr)
+  {
+    ASPLIB_ERR err = ASPLIB_FACTORY_FFT.Create(FFTLibID, FFTInFmt, T::typeID, m_fft);
     if(err != ASPLIB_ERR_NO_ERROR)
     {
       return err;
     }
 
-    err = fft->Create(FrameSize);
+    m_MaxFrameSize = FrameSize;
+    err = m_fft->Create(m_MaxFrameSize*2);
     if (err != ASPLIB_ERR_NO_ERROR)
     {
       return err;
     }
 
-    float *filter = (float*)Filter;
-    m_MaxParts = MaxParts;
-    uint32_t filterPartSize = FilterLength / m_MaxParts;
-    if (filterPartSize*m_MaxParts < FilterLength)
+    // set internal frame size and scale factor
+    m_MaxInteralFrameSize = m_fft->InternalFrameSize();// / T::vectorSize;
+    m_MaxVectorFrameSize = m_MaxInteralFrameSize / T::vectorSize;
+    m_ScaleValue = 1.0 / (double)m_fft->InternalFrameSize();
+
+    // store filter in internal buffer
+    m_MaxParts = FilterLength / m_MaxFrameSize;
+    if (m_MaxFrameSize*m_MaxParts < FilterLength)
     {
       m_MaxParts++;
     }
 
-    m_G = new TFrameBuffer<T>(filterPartSize, MaxParts);
-    for (uint32_t ii = 0; ii < MaxParts; ii++)
+    TFrameBuffer<T::type> *filter = new TFrameBuffer<T::type>(m_fft->InternalFrameSize(), m_MaxParts);
+    filter->ResetBuffer();
+    for (uint32_t ii = 0; ii < m_MaxParts; ii++)
     {
-      m_fft->FFT(&filter[ii*filterPartSize], m_G->get_Frame(ii));
+      T::type *p = filter->get_Frame(ii);
+      for (uint32_t jj = 0; jj < m_MaxFrameSize && ii*m_MaxParts + jj < FilterLength; jj++)
+      {
+        p[jj] = ((T::type*)Filter)[ii*m_MaxParts + jj];
+      }
     }
+
+    // instanciate buffers
+    m_G = new TFrameBuffer<T>(m_MaxInteralFrameSize, m_MaxParts);
+    m_X = new TFrameRingBuffer<T>(m_MaxInteralFrameSize, m_MaxParts);
+    m_Y = new TFrameBuffer<T>(m_MaxInteralFrameSize, m_MaxParts);
+    m_y = new TFrameBuffer<T::type>(m_MaxInteralFrameSize, 1);
+    m_x = new TFrameBuffer<T::type>(m_MaxInteralFrameSize, 1);
+    for (uint32_t ii = 0; ii < m_MaxParts; ii++)
+    {
+      m_fft->FFT(filter->get_Frame(ii), m_G->get_Frame(ii));
+    }
+
+    delete filter;
 
     return ASPLIB_ERR_NO_ERROR;
   }
 
   virtual ASPLIB_ERR Convolve(void *x, void *y)
   {
-    m_fft->FFT(x, m_X->get_NextFrame());
+    T::type *in = m_x->get_Frame(0);
+    copy_n(in + m_MaxInteralFrameSize/2, m_MaxInteralFrameSize/2, in);
+    copy_n((T::type*)x, m_MaxFrameSize, in + m_MaxInteralFrameSize/2);
+
+    m_fft->FFT(m_x->get_Frame(0), m_X->get_NextFrame());
 
     for (uint32_t frame = 0; frame < m_MaxParts; frame++)
     {
@@ -87,7 +128,7 @@ public:
       T *G = m_G->get_Frame(frame);
       T *Y = m_Y->get_Frame(frame);
 
-      for (uint32_t ii = 0; ii < m_MaxFrameSize; ii++)
+      for (uint32_t ii = 0; ii < m_MaxVectorFrameSize; ii++)
       {
         Y[ii] = G[ii]*X[ii];
       }
@@ -97,13 +138,20 @@ public:
     for (uint32_t frame = 1; frame < m_MaxParts; frame++)
     {
       T *Y = m_Y->get_Frame(frame);
-      for (uint32_t ii = 0; ii < m_MaxFrameSize; ii++)
+      for (uint32_t ii = 0; ii < m_MaxVectorFrameSize; ii++)
       {
         Y0[ii] += Y[ii];
       }
     }
 
-    m_fft->iFFT(Y0, y);
+    for (uint32_t ii = 0; ii < m_MaxVectorFrameSize; ii++)
+    {
+      Y0[ii].Scale(m_ScaleValue);
+    }
+
+    m_fft->iFFT(Y0, m_y->get_Frame(0));
+
+    copy_n(m_y->get_Frame(0) + m_MaxInteralFrameSize/2, m_MaxFrameSize, (T::type*)y);
 
     return ASPLIB_ERR_NO_ERROR;
   }
@@ -115,6 +163,8 @@ public:
     delete m_G; m_G = nullptr;
     delete m_X; m_X = nullptr;
     delete m_Y; m_Y = nullptr;
+    delete m_y; m_y = nullptr;
+    delete m_x; m_x = nullptr;
 
     return ASPLIB_ERR_NO_ERROR;
   }
@@ -124,9 +174,14 @@ private:
 
   uint32_t m_MaxParts;
   uint32_t m_MaxFrameSize;
+  uint32_t m_MaxInteralFrameSize;
+  uint32_t m_MaxVectorFrameSize;
+  typename T::type m_ScaleValue;
 
-  TFrameBuffer<T>      *m_G;
-  TFrameRingBuffer<T>  *m_X;
-  TFrameBuffer<T>      *m_Y;
+  TFrameBuffer<T>       *m_G;
+  TFrameRingBuffer<T>   *m_X;
+  TFrameBuffer<T>       *m_Y;
+  TFrameBuffer<typename T::type> *m_y;
+  TFrameBuffer<typename T::type> *m_x;
 };
 }
