1d2c0003f9477436e22f1a2e821133ec01aa10eb
AchimTuran <mail@achim-turan.de>
2016-12-22 21:13:12 +0100

    [asplib_modules][Convolution] Create interface method signature change

--- asplib_modules/Convolution/asplib/Convolution/Interfaces/asplib_IConvolution.hpp
+++ asplib_modules/Convolution/asplib/Convolution/Interfaces/asplib_IConvolution.hpp
@@ -34,7 +34,7 @@ namespace asplib
 class IConvolution
 {
 public:
-  virtual ASPLIB_ERR Create(uint32_t FrameSize, FFTLibID_t FFTLibID, void *Filter, uint32_t FilterLength, uint32_t MaxParts, void *Options = nullptr) = 0;
+  virtual ASPLIB_ERR Create(uint32_t FrameSize, FFTLibID_t FFTLibID, void *Filter, uint32_t FilterLength, void *Options = nullptr) = 0;
   virtual ASPLIB_ERR Destroy() = 0;
   virtual ASPLIB_ERR Convolve(void *x, void *y) = 0;
 };
